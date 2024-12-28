a5b0f6ec9a63ea47ec42c1c56826494dcf14cb1d
Will Finnegan <willfinnegan@wills-air.myfiosgateway.com>
2024-12-28 10:31:28 -0500

    initial commit

---
+++ src/pages/Terminology.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Terminology = () => {
+  return (
+    <div>
+      <h1>Trading Terminology</h1>
+      {/* List of trading terms will go here */}
+    </div>
+  );
+};
+
+export default Terminology;
