f4ce4d28e4e9387dda4ba551326c5b53d60d4fb4
Will Finnegan <wfinnegan13@gmail.com>
2025-02-19 13:17:37 -0500

    update api endpoint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ function Login({ setIsAuthenticated }) {
   const [message, setMessage] = useState('');
 
   // API Endpoint
-  const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT_DEV || 'http://trading-tutor-api-prod.eba-scnpdj3m.us-east-2.elasticbeanstalk.com/' 
+  const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT 
 
   const navigate = useNavigate();
 
